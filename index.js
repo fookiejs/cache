a9f9ee3399bbbea82e6dccf5eb2a325e8678503a
umudik <seyitumutbicer@gmail.com>
2022-01-13 00:54:11 +0300

    dev

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 
-(async () => {
-    const Fookie = require('fookie')
-    const fookie = new Fookie()
-    await fookie.core()
-    await fookie.server()
-    await fookie.use(require("./src/cache-plugin/model/cache"))
-    fookie.listen(26001)
-})()
+const redis = require("redis")
+
+module.exports = async function (ctx) {
+    const client = redis.createClient({
+        url: process.env.REDIS_URL
+    })
+    await ctx.modify({
+        name: "is_cached",
+        function: async function (ctx) {
+            console.log("is_cached");
+        }
+    })
+
+    await ctx.effect({
+        name: "send_to_cache",
+        function: async function (ctx) {
+            console.log("send_to_cache");
+        }
+    })
+
+    let after = ctx.local.get("mixin", "after")
+    after.object.read.modify.push("is_cached")
+    after.object.read.effect.push("send_to_cache")
+
+    await ctx.run({
+        system: true,
+        method: "update",
+        model: "mixin",
+        query: {
+            name: "after"
+        },
+        body: {
+            object: after.object
+        }
+    })
+
+}
 
